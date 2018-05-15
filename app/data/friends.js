6a5db4eb377481a9cc0e7af2065e8c155e73ab84
Grady <Gradyhouston@gmail.com>
2018-05-14 20:41:56 -0400

    pushing up mostly working version of friend-finder app

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: "Ant-Man",
+		photo: "https://i.imgur.com/4r6LhpZ.png",
+		scores: [
+			"1",
+			"5",
+			"5",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Black Panther",
+		photo: "https://i.imgur.com/0mUXsAv.jpg",
+		scores: [
+			"2",
+			"4",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"2",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Captain America",
+		photo: "https://i.imgur.com/XiJS9ev.jpg",
+		scores: [
+			"4",
+			"3",
+			"5",
+			"1",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1"
+		]
+	},
+	{
+		name: "Iron Man",
+		photo: "https://i.imgur.com/Hn4zeNi.jpg",
+		scores: [
+			"5",
+			"1",
+			"2",
+			"1",
+			"3",
+			"4",
+			"1",
+			"3",
+			"3",
+			"4"
+		]
+	},
+	{
+		name: "Spiderman",
+		photo: "https://i.imgur.com/DigQk6p.jpg",
+		scores: [
+			"1",
+			"1",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"1"
+		]
+	},
+	{
+		name: "Thor",
+		photo: "https://i.imgur.com/mZBGJUp.jpg",
+		scores: [
+			"1",
+			"1",
+			"5",
+			"1",
+			"3",
+			"2",
+			"5",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Black Widow",
+		photo: "https://i.imgur.com/p8o9kK5.jpg",
+		scores: [
+			"5",
+			"4",
+			"4",
+			"2",
+			"4",
+			"4",
+			"3",
+			"2",
+			"3",
+			"3"
+		]
+	},
+	{
+		name: "Scarlet Witch",
+		photo: "https://i.imgur.com/XmO8oPG.png",
+		scores: [
+			"5",
+			"2",
+			"3",
+			"1",
+			"5",
+			"1",
+			"3",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "The Hulk",
+		photo: "https://i.imgur.com/YLoAUD2.jpg",
+		scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+		]
+	},
+	{
+		name: "Gamora",
+		photo: "https://i.imgur.com/mWKUIgz.jpg",
+		scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+		]
+	}
+];
+
+module.exports = friendsArray;
